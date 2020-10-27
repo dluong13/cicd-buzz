028a336681be6e4dacdc9ec505791975c6bbec8f
Ubuntu <ubuntu@ip-172-31-49-136.us-west-2.compute.internal>
2020-10-27 21:29:41 +0000

    name added

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
+buzz = ('Daniel Luong', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('A00877970', 'complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
